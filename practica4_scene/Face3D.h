654cc93ce051f68e842f2ac65bc92da9f0e77230
Gerardo <Gerardo@treeweb-7c19d67>
2012-03-14 13:58:12 +0100

    traslaci?n de marcos de frenet

--- practica4_scene/Face3D.h
+++ practica4_scene/Face3D.h
@@ -3,18 +3,10 @@
 
 class Face3D {
 public:
-        int numVertices;
-        double faceSize;
-        Face3D();
-        double faceSize();
 
 
 
 private:
-        String _name;
-        void resetFace();
-
-        void newPrivateMethod(int with_very_fucking_results);
 
 
 };
