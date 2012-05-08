5edc1bd54a0b791a7c844d5c5508ebb9b738df44
Gerardo <Gerardo@treeweb-7c19d67>
2012-05-09 01:28:18 +0200

    Detalles

--- practica4_scene/EventZRotate.cpp
+++ practica4_scene/EventZRotate.cpp
@@ -18,12 +18,14 @@ bool EventZRotate::event(ViewPort3D *_viewport, Scene3D *_scene, String type, WO
         if (type=="KeyDown") {
 
                 // Para controlar la velocidad de giro con Control y Shift
+                /*
                 double velocity = 1; // Velocidad normal
                 if (shift.Contains(ssShift)) {
                         velocity = 10; // Velocidad alta
                 } else if (shift.Contains(ssCtrl)) {
                         velocity = 0.1; // Velocidad lenta
                 }
+                */
 
                 if (key==38) { // flecha arriba
                         _viewport->camera->zRotate(true);
